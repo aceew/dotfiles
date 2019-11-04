25c83c108be8122b49dc7c4f98b7ce814c498e58
aceew <aaronwilliams9797@gmail.com>
2019-11-04 13:39:18 +0000

    feat: changed dock size

--- macos/mac-settings.sh
+++ macos/mac-settings.sh
@@ -4,7 +4,7 @@
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 # Dock Settings
-defaults write com.apple.dock tilesize -int 40
+defaults write com.apple.dock tilesize -int 48
 defaults write com.apple.dock mineffect -string "scale"
 defaults write com.apple.dock show-recents -bool false
 defaults write -g com.apple.mouse.scaling 5
