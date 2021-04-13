f33c7f0d861c25b184dc31697921cd051b35b577
aceew <aaronwilliams9797@gmail.com>
2021-04-13 11:29:24 +0100

    fix: updated method of installing brew and added brave

--- macos/run.sh
+++ macos/run.sh
@@ -4,7 +4,7 @@ BASEDIR=$(dirname "$0")
 
 if ! type "brew" > /dev/null; then
 	echo "Installing homebrew"
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	sudo xcode-select --install
 fi
 
