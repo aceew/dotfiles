78e86b4941f7f01c46ee59cca590adcc556c8aba
aceew <aaronwilliams9797@gmail.com>
2022-05-27 13:56:55 +0100

    fix: removed sylink from aws file as it was constantly biting me

--- macos/aws/setup.sh
+++ macos/aws/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-AWS_SHARED_CREDENTIALS_FOLDER=~/.secrets/.aws/
-AWS_SHARED_CREDENTIALS_FILE="${AWS_SHARED_CREDENTIALS_FOLDER}credentials"
+AWS_SHARED_CREDENTIALS_FILE=~/.aws/credentials
 
 mkdir -p ~/.aws
 
@@ -9,6 +8,5 @@ if [ ! -d $AWS_SHARED_CREDENTIALS_FOLDER ]; then
   echo "You will need to set up your AWS credentials file at: $AWS_SHARED_CREDENTIALS_FILE"
   mkdir -p "$AWS_SHARED_CREDENTIALS_FOLDER"
   touch $AWS_SHARED_CREDENTIALS_FILE
-  ln -s "$AWS_SHARED_CREDENTIALS_FILE" ~/.aws/credentials
   ln -s "${PWD}/config" ~/.aws/config
 fi
